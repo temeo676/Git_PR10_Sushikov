3cc25156968cf533cea2dccba284af9894366d67
Brunov <Brunov@gmail.com>
2025-11-13 09:15:24 +0300

    bugfix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine($"Текущая дата: {DataTime.Now.ToString()}");
+Console.WriteLine($"Текущая дата: {DateTime.Now.ToString()}");
